1f008e56a405384ed26d9aa8b179a3244e1f469a
Alberto Gualis <agualis@gmail.com>
2016-02-01 20:42:19 +0100

    Added Replace with method object smell

---
+++ src/com/agualis/refactoring/bloated/Order.java
@@ -0,0 +1,38 @@
+package com.agualis.refactoring.bloated;
+
+class Order {
+
+    private int foo;
+    private int baz;
+    private int bar;
+
+    public Order() {
+        foo = 23;
+        bar = 34;
+        baz = 9;
+    }
+
+    public double price() {
+        double primaryBasePrice = 23;
+        double secondaryBasePrice = 34;
+        double tertiaryBasePrice = 54;
+
+
+        // long computation with intertwined local variables
+
+        for (int i = 0; i < 13; i++) {
+            primaryBasePrice += this.bar;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            tertiaryBasePrice -= this.foo/2;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            secondaryBasePrice *= this.baz +3;
+        }
+
+        return primaryBasePrice / secondaryBasePrice - tertiaryBasePrice;
+
+    }
+}
---
+++ test/com/agualis/refactoring/bloated/TestReplaceWithMethodObject.java
@@ -0,0 +1,13 @@
+package com.agualis.refactoring.bloated;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class TestReplaceWithMethodObject {
+
+    @org.junit.Test public void
+    test() {
+        assertThat(new Order().price(), is(232.00005496266795));
+    }
+
+}
